3f26b4c19e4cfc472d0556fdd3e2b7d208b3c808
tomharvey <tom@alush.co.uk>
2015-01-03 02:10:02 +0000

    adding soem docs to the main classes

--- lib/rsweb/connection.rb
+++ lib/rsweb/connection.rb
@@ -1,13 +1,31 @@
 require 'fog'
 
+# Handle the connection to the object storage service
+#
 class Connection
   include Logging
   attr_reader :container
 
+  # Create a conneciton
+  #
   def initialize
     @conx = Fog::Storage.new(Settings.connection)
   end
 
+  # Manages the container used for the static website
+  # If no container exists with that name, a new one is created
+  # If a container exists with that name it is returned.
+  # <b>If a container exists with that name, but the metadata differs,
+  # the metadata will be updated with the metadata argued here.</b>
+  #
+  # The container becomes atached to the instance of this class
+  # 
+  # * *Args*    :
+  #   - +container_name+ -> String name of the container
+  #   - +metadata+ -> Hash of metadata about the container
+  # * *Returns* :
+  #   - A Fog::Directory object aka a rackfiles container
+  #
   def get_create_container(container_name, metadata)
     unless metadata
       metadata = {:web_index=>"index.html",
@@ -22,6 +40,15 @@ class Connection
     @container = @conx.directories.create(options)
   end
 
+  # Uploads a file to the container attached to the instance of this class
+  # self.get_create_container must be ran first to setup the traget container.
+  # 
+  # * *Args*    :
+  #   - +filepath+ -> String path to the file within the project
+  #   - +project_path+ -> String absolute path to the project
+  # * *Returns* :
+  #   - A Fog::File object representing the now remote file
+  #
   def upload_file(filepath, project_path)
     data = File.open([project_path, filepath].join("/")).read
     md5 = Digest::MD5.hexdigest(data)
@@ -33,6 +60,13 @@ class Connection
 
   protected
 
+  # Clean the name of the file, ready for upload
+  # 
+  # * *Args*    :
+  #   - +filepath+ -> String path to the file within the project
+  # * *Returns* :
+  #   - A String representing a filepath, suitable for upload
+  #
   def clean_name(filepath)
     return filepath
   end
--- lib/rsweb/project.rb
+++ lib/rsweb/project.rb
@@ -1,24 +1,49 @@
 require 'yaml'
 require 'grit'
 
+# Manages the local files of the website project
+#
 class Project
   include Logging
 
-  def initialize(project_path = Dir.pwd)
+  # Setup a website project.
+  #
+  # * *Args*    :
+  #   - +project_path+ -> String path to the project, defaults to pwd
+  #
+  def initialize(project_path)
+    project_path ||= Dir.pwd
     self.get_head(project_path)
     self.load_project_settings(project_path)
   end
 
+  # Configure the name of the website project.
+  # The name should be taken from a .rsweb YAML file in the project root.
+  #
+  # * *Returns* :
+  #   - A String name
+  #
   def name
     @config["project_name"]
   end
 
+  # Get a list of files checked into git.
+  # We only manage files which are checked in.
+  # 
+  # * *Returns* :
+  #   - An Array of filenames, relative to the project root
+  #
   def checked_in_files
     @recursed_tree ||= self.recurse_tree(@head, "")
   end
 
   protected
 
+  # Get the project config from the .rsweb YAML file on the project root
+  # 
+  # * *Returns* :
+  #   - A Boolean of whether the .rsweb file is commited to git
+  #
   def load_project_settings(project_path)
     config_file = [project_path, ".rsweb"].join("/")
 
@@ -30,6 +55,11 @@ class Project
     end
   end
 
+  # Check if the .rsweb file is commited to the the repo
+  # 
+  # * *Returns* :
+  #   - A Boolean of whether the .rsweb file is commited to git
+  #
   def config_is_checked_in
     config_in_head = @head / ".rsweb"
     if not config_in_head
@@ -38,6 +68,13 @@ class Project
     return config_in_head
   end
 
+  # Get the git master tree
+  # 
+  # * *Args*    :
+  #   - +project_path+ -> String path to the project
+  # * *Returns* :
+  #   - A Grit::Tree object - the current master tree of the repo
+  #
   def get_head(project_path)
     if not @head
       repo = Grit::Repo.new(project_path)
@@ -45,6 +82,14 @@ class Project
     end
   end
 
+  # Traverse the tree and build a list of files
+  # 
+  # * *Args*    :
+  #   - +tree+ -> A Grit::Tree object to be traversed
+  #   - +pathname+ -> The String of the path to that Tree's root
+  # * *Returns* :
+  #   - An Array of filenames, relative to the project root
+  #
   def recurse_tree(tree, pathname)
     @recursed_tree ||= []
     for blob in tree.blobs
